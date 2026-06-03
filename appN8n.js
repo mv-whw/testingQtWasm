0378b34e2bc1ba06f2a5da50b1e7c1828d1af173
Miran Višner <miran.visner@gmail.com>
2026-06-03 19:08:45 +0200

    bugfixes

--- appN8n.js
+++ appN8n.js
@@ -13474,7 +13474,7 @@ function checkIncomingModuleAPI() {
 }
 
 var ASM_CONSTS = {
-  10835513: () => {
+  10841273: () => {
     Module.qtSuspendResumeControl = ({
       resume: null,
       asyncifyEnabled: false,
@@ -13483,7 +13483,7 @@ var ASM_CONSTS = {
       exclusiveEventHandler: 0
     });
   },
-  10835658: $0 => {
+  10841418: $0 => {
     function createNamedFunction(name, parent, obj) {
       return {
         [name]: function(...args) {
